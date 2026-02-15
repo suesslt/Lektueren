e76466376487c9ba00f84fe883b708f031bfeaf4
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-15 11:51:05 +0900

    Next step

--- GenericTreeModel/Model/TreeItem.swift
+++ GenericTreeModel/Model/TreeItem.swift
@@ -4,11 +4,8 @@
 //
 //  Created by Thomas Süssli on 15.02.2026.
 //
-
 import Foundation
 
-// KEIN Hashable/Identifiable hier!
-@MainActor
 protocol TreeItem: AnyObject {
     var id: UUID { get }
     var name: String { get }
