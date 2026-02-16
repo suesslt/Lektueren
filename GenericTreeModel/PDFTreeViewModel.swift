4b5afe3b6544ff0f43d82988c5ec44fb7d3eed48
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-16 18:41:38 +0900

    Enhanced examples

--- GenericTreeModel/PDFTreeViewModel.swift
+++ GenericTreeModel/PDFTreeViewModel.swift
@@ -22,9 +22,10 @@ class PDFTreeViewModel: TreeViewModel {
     private func loadSampleData() {
         let pdf1 = PDFItem(title: "Asia report", fileName: "Rechnung_Januar.pdf", author: "Michael Brabeck", pageCount: 18, fileSize: "1.2 MB", pdfUrl: URL(string: "https://example.com/1.pdf"))
         let pdf2 = PDFItem(title: "Blick Bericht", fileName: "Vertrag_final.pdf", author: "John Sullivan", pageCount: 129, fileSize: "4.5 MB", pdfUrl: URL(string: "https://example.com/2.pdf"))
+        let pdf3 = PDFItem(title: "Ukraine Story", fileName: "Ukraine_at_the_trenches.pdf", author: "Mark Hubbard", pageCount: 4, fileSize: "20 KB", pdfUrl: URL(string: "https://example.com/ukraine.pdf"))
 
         let workFolder = PDFFolder(name: "Arbeit", icon: "folder.fill", items: [pdf1, pdf2])
-        let privateFolder = PDFFolder(name: "Privat", icon: "folder.fill")
+        let privateFolder = PDFFolder(name: "Privat", icon: "folder.fill", items: [pdf3])
 
         let root = PDFFolder(
             name: "Meine Dokumente",
