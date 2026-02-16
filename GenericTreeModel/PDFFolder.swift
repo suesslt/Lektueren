34a0bd35107c20435f367e75c5dab726867bd9ad
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-16 15:20:46 +0900

    Small improvements

---
+++ GenericTreeModel/PDFFolder.swift
@@ -0,0 +1,37 @@
+//
+//  PDFFolder.swift
+//  GenericTreeModel
+//
+//  Created by Thomas Süssli on 16.02.2026.
+//
+import SwiftUI
+import SwiftData
+
+@Model
+final class PDFFolder: TreeFolderItem {
+    @Attribute(.unique) var id: UUID = UUID()
+    var name: String
+    var icon: String
+
+    @Relationship(deleteRule: .cascade, inverse: \PDFItem.folder)
+    var items: [PDFItem]?
+
+    var parent: PDFFolder?
+
+    @Relationship(deleteRule: .cascade, inverse: \PDFFolder.parent)
+    var subfolders: [PDFFolder]?
+
+    init(
+        name: String,
+        icon: String = "folder.fill",
+        items: [PDFItem]? = nil,
+        parent: PDFFolder? = nil,
+        subfolders: [PDFFolder]? = nil
+    ) {
+        self.name = name
+        self.icon = icon
+        self.items = items
+        self.parent = parent
+        self.subfolders = subfolders
+    }
+}
